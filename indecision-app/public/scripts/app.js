4cf4841a75705c52eb45f7d0c8617c737b014eb4
Ameya Raje <ameyacraje@gmail.com>
2020-02-19 20:19:21 -0600

    S3-18: ES6 stuff done and basic template for indecision completed

--- indecision-app/public/scripts/app.js
+++ indecision-app/public/scripts/app.js
@@ -2,31 +2,81 @@
 
 var app = {
     title: 'Indecision App',
-    // subtitle: 'You are now controlled by a computer',
+    subtitle: 'You are now controlled by a computer',
     options: ['One', 'Two']
 };
 
-var template = React.createElement(
-    'div',
-    null,
-    React.createElement(
-        'h1',
-        null,
-        app.title
-    ),
-    app.subtitle && React.createElement(
-        'p',
-        null,
-        app.subtitle
-    ),
-    app.options.length > 0 ? "Here are your options" : "No options specified",
-    React.createElement(
-        'ol',
-        null,
-        React.createElement('li', null),
-        React.createElement('li', null)
-    )
-);
+var onFormSubmit = function onFormSubmit(e) {
+    e.preventDefault();
+
+    var option = e.target.elements.option.value;
+    if (option) {
+        app.options.push(option);
+        e.target.elements.option.value = '';
+        renderIndecisionApp();
+    }
+};
+
+var deleteOptions = function deleteOptions() {
+    app.options = [];
+    renderIndecisionApp();
+};
 
 var appRoot = document.getElementById('app');
-ReactDOM.render(template, appRoot);
+
+var renderIndecisionApp = function renderIndecisionApp() {
+    var template = React.createElement(
+        'div',
+        null,
+        React.createElement(
+            'h1',
+            null,
+            app.title
+        ),
+        app.subtitle && React.createElement(
+            'p',
+            null,
+            app.subtitle
+        ),
+        app.options.length > 0 ? "Here are your options" : "No options specified",
+        React.createElement(
+            'p',
+            null,
+            'Number of options: ',
+            app.options.length
+        ),
+        React.createElement(
+            'button',
+            { onClick: deleteOptions },
+            'Remove Options'
+        ),
+        React.createElement(
+            'ol',
+            null,
+            React.createElement(
+                'li',
+                null,
+                'Item One'
+            ),
+            React.createElement(
+                'li',
+                null,
+                'Item Two'
+            )
+        ),
+        React.createElement(
+            'form',
+            { onSubmit: onFormSubmit },
+            React.createElement('input', { type: 'text', name: 'option' }),
+            React.createElement(
+                'button',
+                null,
+                'Add Option'
+            )
+        )
+    );
+
+    ReactDOM.render(template, appRoot);
+};
+
+renderIndecisionApp();
--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -1,20 +1,48 @@
 var app = {
     title: 'Indecision App',
-    // subtitle: 'You are now controlled by a computer',
+    subtitle: 'You are now controlled by a computer',
     options: ['One', 'Two']
 };
 
-var template = (
-    <div>
-        <h1>{app.title}</h1>
-        {app.subtitle && <p>{app.subtitle}</p>}
-        {app.options.length > 0 ? "Here are your options" : "No options specified"}
-        <ol>
-            <li></li>
-            <li></li>
-        </ol>
-    </div>
-);
+const onFormSubmit = (e) => {
+    e.preventDefault();
+
+    const option = e.target.elements.option.value;
+    if (option) {
+        app.options.push(option);
+        e.target.elements.option.value = '';
+        renderIndecisionApp();
+    }
+    
+};
+
+const deleteOptions = () => {
+    app.options = [];
+    renderIndecisionApp();
+};
 
 var appRoot = document.getElementById('app');
-ReactDOM.render(template, appRoot);
+
+const renderIndecisionApp = () => {
+    var template = (
+        <div>
+            <h1>{app.title}</h1>
+            {app.subtitle && <p>{app.subtitle}</p>}
+            {app.options.length > 0 ? "Here are your options" : "No options specified"}
+            <p>Number of options: {app.options.length}</p>
+            <button onClick={deleteOptions}>Remove Options</button>
+            <ol>
+                <li>Item One</li>
+                <li>Item Two</li>
+            </ol>
+            <form onSubmit={onFormSubmit}>
+                <input type="text" name="option"></input>
+                <button>Add Option</button>
+            </form>
+        </div>
+    );
+
+    ReactDOM.render(template, appRoot);
+};
+
+renderIndecisionApp();

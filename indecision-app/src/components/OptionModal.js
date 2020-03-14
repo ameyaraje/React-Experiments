6339db02fae1ab436ab58dc624e934fc298c4fba
Ameya Raje <ameyacraje@gmail.com>
2020-03-13 21:03:51 -0500

    S8-71: indecision app done

--- indecision-app/src/components/OptionModal.js
+++ indecision-app/src/components/OptionModal.js
@@ -3,10 +3,10 @@ import Modal from 'react-modal';
 
 const OptionModal = (props) => {
     return (
-        <Modal isOpen={!!props.selectedOption} contentLabel="Selected Option" onRequestClose={props.clearSelectedOption}>
-            <h2>Selected Option</h2>
-            {props.selectedOption && <p>{props.selectedOption}</p>}
-            <button onClick={props.clearSelectedOption}>Okay</button>
+        <Modal className="modal" isOpen={!!props.selectedOption} contentLabel="Selected Option" onRequestClose={props.clearSelectedOption} closeTimeoutMS={200}>
+            <h2 className="modal__title">Selected Option</h2>
+            {props.selectedOption && <p className="modal__body">{props.selectedOption}</p>}
+            <button className="button" onClick={props.clearSelectedOption}>Okay</button>
         </Modal>
     );
 };

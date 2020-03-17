5c191d0049d24c6b4d0b0684c9fa34f994a0c158
Ameya Raje <ameyacraje@gmail.com>
2020-03-16 20:48:33 -0500

    S10-87: started with redux; basic stuff accomplished

--- expensify-app/webpack.config.js
+++ expensify-app/webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-    entry: './src/app.js',
+    entry: './src/playground/S10-85_redux_basics.js',
     output: {
         // path: '/Users/ameyaraje/Desktop/ReactJS/React-Experiments/indecision-app/public',
         // usage of 'path' makes more sense so the project is portable

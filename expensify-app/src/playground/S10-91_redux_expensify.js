b17911a139d4b2cbb608c5aa238113ff9d209089
Ameya Raje <ameyacraje@gmail.com>
2020-03-23 21:09:52 -0400

    S10-97: added sorting and filtering to expensify; redux study now complete

--- expensify-app/src/playground/S10-91_redux_expensify.js
+++ expensify-app/src/playground/S10-91_redux_expensify.js
@@ -23,7 +23,7 @@ const filtersReducerDefaultState = {
 };
 
 
-// actions
+// action generators
 const addExpense = ({ description = '', note = '', amount = 0, createdAt = 0 } = {}) => {
     return {
         type: 'ADD_EXPENSE',
@@ -153,6 +153,25 @@ const filtersReducer = (state = filtersReducerDefaultState, action) => {
     }
 };
 
+// get visible expenses
+
+const getVisibleExpenses = (expenses, { text, sortBy, startDate, endDate }) => {
+    return expenses.filter((expense) => {
+        const startDateMatch = typeof startDate !== 'number' || expense.createdAt >= startDate;
+        const endDateMatch = typeof endDate !== 'number' || expense.createdAt <= endDate;
+        const textMatch = expense.description.toLowerCase().includes(text.toLowerCase());
+
+        return startDateMatch && endDateMatch && textMatch;
+    }).sort((a, b) => {
+        if (sortBy === 'date') {
+            return a.createdAt < b.createdAt ? 1 : -1;
+        }
+        else if (sortBy === 'amount') {
+            return a.amount < b.amount ? 1 : -1;
+        }
+    });
+};
+
 // create store
 const store = createStore(combineReducers({
     expenses: expensesReducer,
@@ -160,13 +179,16 @@ const store = createStore(combineReducers({
 }));
 
 store.subscribe(() => {
-    console.log(store.getState());
+    const state = store.getState();
+    const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
+    console.log(visibleExpenses);
 });
 
 const exp1 = store.dispatch(addExpense({
     description: 'Test',
     note: 'testest',
-    amount: 1400
+    amount: 1400,
+    createdAt: 10000
 }));
 
 const exp2 = store.dispatch(addExpense({
@@ -175,22 +197,22 @@ const exp2 = store.dispatch(addExpense({
     amount: 500
 }));
 
-store.dispatch(removeExpense({
-    id: exp1.expense.id
-}));
+// store.dispatch(removeExpense({
+//     id: exp1.expense.id
+// }));
 
-store.dispatch(editExpense(exp2.expense.id, {
-    note: 'new edited value'
-}));
+// store.dispatch(editExpense(exp2.expense.id, {
+//     note: 'new edited value'
+// }));
 
-store.dispatch(setTextFilter('rent'));
-store.dispatch(setTextFilter());
+// store.dispatch(setTextFilter('tes'));
+// store.dispatch(setTextFilter());
 store.dispatch(sortByAmount());
-store.dispatch(sortByDate());
-store.dispatch(setStartDate(100));
-store.dispatch(setStartDate());
-store.dispatch(setEndDate(11111));
-store.dispatch(setEndDate());
+// store.dispatch(sortByDate());
+// store.dispatch(setStartDate(1000));
+// store.dispatch(setStartDate());
+// store.dispatch(setEndDate(11111));
+// store.dispatch(setEndDate());
 
 
 
